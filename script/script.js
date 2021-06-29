70bc2bece3cdcfa9d3f699877316026078729ec4
praisejah uzoma <praisejah.c.uzoma@gmail.com>
2021-06-29 17:03:11 +0100

    get the data of each of the 10 users using fetch api

--- script/script.js
+++ script/script.js
@@ -11,9 +11,20 @@ let names = [
 	{firstName: "Zara", lastName: " Athena"},
 ]
 
-names.forEach(function(item) {
-  var li = document.createElement("li");
-  var text = document.createTextNode(`${item.firstName} ${item.lastName}`);
-  li.appendChild(text);
-  document.getElementById("nameList").appendChild(li);
-});
+let names2
+
+fetch('https://jsonplaceholder.typicode.com/users/')
+					.then(res => res.json())
+					.then(data => {
+						console.log(data)
+						names2 = data;
+						names2.forEach(function(item) {
+							var li = document.createElement("li");
+							var text = document.createTextNode(`${item.name} works at ${item.company.name} and live in ${item.address.city}`);
+							li.appendChild(text);
+							document.getElementById("nameList").appendChild(li);
+						  });
+					})
+console.log(names2)
+
+
